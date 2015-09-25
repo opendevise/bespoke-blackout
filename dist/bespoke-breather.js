67e62e58e526547c424f47332c623bc69e34d6c3
Dan Allen <dan.j.allen@gmail.com>
2015-09-25 00:26:43 -0600

    consolidate and optimize code
    - only register events to prevent navigation when pause is active
    - use more concise constants
    - change pause method to togglePause

--- dist/bespoke-breather.js
+++ dist/bespoke-breather.js
@@ -1,5 +1,5 @@
 /*!
- * bespoke-breather v1.0.0
+ * bespoke-breather v1.0.0-dev
  *
  * Copyright 2015, Dan Allen
  * This content is released under the MIT license
@@ -8,42 +8,30 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.bespoke||(g.bespoke = {}));g=(g.plugins||(g.plugins = {}));g.breather = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 module.exports = function() {
   return function(deck) {
-    var KEYCODE = { b: 66, period: 190 },
+    var KEY_B = 66, KEY_DOT = 190, EVT_KEYDOWN = 'keydown',
       paused = false,
-      pause = function(toState) {
+      togglePause = function() {
         var parentClasses = deck.parent.classList;
-        if (toState === false) {
-          if (paused) {
-            parentClasses.remove('bespoke-paused');
-            paused = false;
-          }
+        if (paused) {
+          parentClasses.remove('bespoke-paused');
+          (paused || []).forEach(function(unbindEvent) { unbindEvent(); });
+          paused = false;
         }
         else {
-          if (!paused) {
-            parentClasses.add('bespoke-paused');
-            paused = true;
-          }
+          parentClasses.add('bespoke-paused');
+          paused = [deck.on('next', preventNavigation), deck.on('prev', preventNavigation)];
         }
       },
-      next = function() {
-        return !paused;
+      preventNavigation = function() {
+        return false;
       },
-      prev = function() {
-        return !paused;
-      },
-      keydownHandler = function(e) {
-        if (e.which === KEYCODE.b || e.which === KEYCODE.period) pause(!paused);
-      },
-      destroy = function() {
-        document.removeEventListener('keydown', keydownHandler, false);
+      onKeydown = function(e) {
+        var key = e.which;
+        if (key === KEY_B || key === KEY_DOT) togglePause();
       };
-
-    document.addEventListener('keydown', keydownHandler, false);
-
-    deck.on('next', next);
-    deck.on('prev', prev);
-    deck.on('pause', pause);
-    deck.on('destroy', destroy);
+    deck.on('pause', togglePause);
+    deck.on('destroy', function() { document.removeEventListener(EVT_KEYDOWN, onKeydown); });
+    document.addEventListener(EVT_KEYDOWN, onKeydown);
   };
 };
 
