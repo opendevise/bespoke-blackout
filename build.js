84647907f1492317ad41ef28a73410c5fecba829
Dan Allen <dan.j.allen@gmail.com>
2015-09-25 22:06:00 -0600

    Updates

---
+++ build.js
@@ -0,0 +1,280 @@
+/*!
+ * Bespoke.js v1.0.0
+ *
+ * Copyright 2014, Mark Dalgleish
+ * This content is released under the MIT license
+ * http://mit-license.org/markdalgleish
+ */
+
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var f;"undefined"!=typeof window?f=window:"undefined"!=typeof global?f=global:"undefined"!=typeof self&&(f=self),f.bespoke=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+var from = function(selectorOrElement, plugins) {
+  var parent = selectorOrElement.nodeType === 1 ? selectorOrElement : document.querySelector(selectorOrElement),
+    slides = [].filter.call(parent.children, function(el) { return el.nodeName !== 'SCRIPT'; }),
+    activeSlide = slides[0],
+    listeners = {},
+
+    activate = function(index, customData) {
+      if (!slides[index]) {
+        return;
+      }
+
+      fire('deactivate', createEventData(activeSlide, customData));
+      activeSlide = slides[index];
+      fire('activate', createEventData(activeSlide, customData));
+    },
+
+    slide = function(index, customData) {
+      if (arguments.length) {
+        fire('slide', createEventData(slides[index], customData)) && activate(index, customData);
+      } else {
+        return slides.indexOf(activeSlide);
+      }
+    },
+
+    step = function(offset, customData) {
+      var slideIndex = slides.indexOf(activeSlide) + offset;
+
+      fire(offset > 0 ? 'next' : 'prev', createEventData(activeSlide, customData)) && activate(slideIndex, customData);
+    },
+
+    on = function(eventName, callback) {
+      (listeners[eventName] || (listeners[eventName] = [])).push(callback);
+
+      return function() {
+        listeners[eventName] = listeners[eventName].filter(function(listener) {
+          return listener !== callback;
+        });
+      };
+    },
+
+    fire = function(eventName, eventData) {
+      return (listeners[eventName] || [])
+        .reduce(function(notCancelled, callback) {
+          return notCancelled && callback(eventData) !== false;
+        }, true);
+    },
+
+    createEventData = function(el, eventData) {
+      eventData = eventData || {};
+      eventData.index = slides.indexOf(el);
+      eventData.slide = el;
+      return eventData;
+    },
+
+    deck = {
+      on: on,
+      fire: fire,
+      slide: slide,
+      next: step.bind(null, 1),
+      prev: step.bind(null, -1),
+      parent: parent,
+      slides: slides
+    };
+
+  (plugins || []).forEach(function(plugin) {
+    plugin(deck);
+  });
+
+  activate(0);
+
+  return deck;
+};
+
+module.exports = {
+  from: from
+};
+
+},{}]},{},[1])
+(1)
+});
+/*!
+ * bespoke-classes v1.0.0
+ *
+ * Copyright 2014, Mark Dalgleish
+ * This content is released under the MIT license
+ * http://mit-license.org/markdalgleish
+ */
+
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var n;"undefined"!=typeof window?n=window:"undefined"!=typeof global?n=global:"undefined"!=typeof self&&(n=self);var o=n;o=o.bespoke||(o.bespoke={}),o=o.plugins||(o.plugins={}),o.classes=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+module.exports = function() {
+  return function(deck) {
+    var addClass = function(el, cls) {
+        el.classList.add('bespoke-' + cls);
+      },
+
+      removeClass = function(el, cls) {
+        el.className = el.className
+          .replace(new RegExp('bespoke-' + cls +'(\\s|$)', 'g'), ' ')
+          .trim();
+      },
+
+      deactivate = function(el, index) {
+        var activeSlide = deck.slides[deck.slide()],
+          offset = index - deck.slide(),
+          offsetClass = offset > 0 ? 'after' : 'before';
+
+        ['before(-\\d+)?', 'after(-\\d+)?', 'active', 'inactive'].map(removeClass.bind(null, el));
+
+        if (el !== activeSlide) {
+          ['inactive', offsetClass, offsetClass + '-' + Math.abs(offset)].map(addClass.bind(null, el));
+        }
+      };
+
+    addClass(deck.parent, 'parent');
+    deck.slides.map(function(el) { addClass(el, 'slide'); });
+
+    deck.on('activate', function(e) {
+      deck.slides.map(deactivate);
+      addClass(e.slide, 'active');
+      removeClass(e.slide, 'inactive');
+    });
+  };
+};
+
+},{}]},{},[1])
+(1)
+});
+/*!
+ * bespoke-nav v1.0.1
+ *
+ * Copyright 2015, Dan Allen
+ * This content is released under the MIT license
+ */
+
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.bespoke||(g.bespoke = {}));g=(g.plugins||(g.plugins = {}));g.nav = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = function(opts) {
+  opts = opts || {};
+  var kbd = require('bespoke-nav-kbd')(opts.kbd);
+  var touch = require('bespoke-nav-touch')(opts.touch);
+  return function(deck) {
+    kbd(deck);
+    touch(deck);
+  };
+};
+
+},{"bespoke-nav-kbd":2,"bespoke-nav-touch":3}],2:[function(require,module,exports){
+module.exports = function() {
+  return function(deck) {
+    var KEY_SB = 32, KEY_PGUP = 33, KEY_PGDN = 34, KEY_END = 35, KEY_HME = 36,
+        KEY_LT = 37, KEY_RT = 39, KEY_H = 72, KEY_L = 76,
+      isModifierPressed = function(e, keyCode) {
+        return e.ctrlKey || (e.shiftKey && keyCode !== KEY_SB) || e.altKey || e.metaKey;
+      },
+      onKeydown = function(e) {
+        if (!isModifierPressed(e, e.which)) {
+          switch(e.which) {
+            case KEY_SB: return e.shiftKey ? deck.prev() : deck.next();
+            case KEY_RT: case KEY_PGDN: case KEY_L: return deck.next();
+            case KEY_LT: case KEY_PGUP: case KEY_H: return deck.prev();
+            case KEY_HME: return deck.slide(0);
+            case KEY_END: return deck.slide(deck.slides.length - 1);
+          }
+        }
+      };
+    deck.on('destroy', function() { document.removeEventListener('keydown', onKeydown); });
+    document.addEventListener('keydown', onKeydown);
+  };
+};
+
+},{}],3:[function(require,module,exports){
+module.exports = function(opts) {
+  return function(deck) {
+    opts = opts || {};
+    var TOUCHSTART = 'touchstart', TOUCHMOVE = 'touchmove', start = null,
+      axis = 'page' + (opts.axis && ['x', 'y'].indexOf(opts.axis) !== -1 ? opts.axis.toUpperCase() : 'X'),
+      gap = (typeof opts.threshold === 'number' ? Math.abs(opts.threshold) : Math.ceil(50 / window.devicePixelRatio)),
+      onTouchstart = function(e) {
+        if (e.touches.length === 1) start = e.touches[0][axis];
+      },
+      onTouchmove = function(e) {
+        if (start === null) return;
+        var delta = e.touches[0][axis] - start;
+        if (Math.abs(delta) > gap) {
+          deck[delta > 0 ? 'prev' : 'next']();
+          start = null;
+        }
+      };
+    deck.on('destroy', function() {
+      deck.parent.removeEventListener(TOUCHSTART, onTouchstart);
+      deck.parent.removeEventListener(TOUCHMOVE, onTouchmove);
+    });
+    deck.parent.addEventListener(TOUCHSTART, onTouchstart);
+    deck.parent.addEventListener(TOUCHMOVE, onTouchmove);
+  };
+};
+
+},{}]},{},[1])(1)
+});
+/*!
+ * bespoke-blackout v1.0.0-dev
+ *
+ * Copyright 2015, Dan Allen
+ * This content is released under the MIT license
+ */
+
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g=(g.bespoke||(g.bespoke = {}));g=(g.plugins||(g.plugins = {}));g.blackout = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = function() {
+  return function(deck) {
+    var KEY_B = 66, KEY_DOT = 190, EVT_KEYDOWN = 'keydown',
+      blackout = false,
+      toggleBlackout = function() {
+        var parentClasses = deck.parent.classList;
+        if (blackout) {
+          parentClasses.remove('bespoke-blackout');
+          (blackout || []).forEach(function(unbindEvent) { unbindEvent(); });
+          blackout = false;
+        }
+        else {
+          parentClasses.add('bespoke-blackout');
+          blackout = [deck.on('next', preventDefault), deck.on('prev', preventDefault)];
+        }
+      },
+      preventDefault = function() { return false; },
+      isModifierPressed = function(e) {
+        return !!(e.ctrlKey || e.shiftKey || e.altKey || e.metaKey);
+      },
+      onKeydown = function(e) {
+        var key = e.which;
+        if ((key === KEY_B || key === KEY_DOT) && !isModifierPressed(e)) toggleBlackout();
+      };
+    deck.on('blackout', toggleBlackout);
+    deck.on('destroy', function() { document.removeEventListener(EVT_KEYDOWN, onKeydown); });
+    document.addEventListener(EVT_KEYDOWN, onKeydown);
+  };
+};
+
+},{}]},{},[1])(1)
+});
+/*!
+ * bespoke-forms v1.0.0
+ *
+ * Copyright 2014, Mark Dalgleish
+ * This content is released under the MIT license
+ * http://mit-license.org/markdalgleish
+ */
+
+!function(e){if("object"==typeof exports&&"undefined"!=typeof module)module.exports=e();else if("function"==typeof define&&define.amd)define([],e);else{var o;"undefined"!=typeof window?o=window:"undefined"!=typeof global?o=global:"undefined"!=typeof self&&(o=self);var f=o;f=f.bespoke||(f.bespoke={}),f=f.plugins||(f.plugins={}),f.forms=e()}}(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+module.exports = function() {
+  return function(deck) {
+    deck.slides.forEach(function(slide) {
+      slide.addEventListener('keydown', function(e) {
+        if (/INPUT|TEXTAREA|SELECT/.test(e.target.nodeName) || e.target.contentEditable === 'true') {
+          e.stopPropagation();
+        }
+      });
+    });
+  };
+};
+
+},{}]},{},[1])
+(1)
+});
+var deck = bespoke.from('.deck', [
+  bespoke.plugins.classes(),
+  bespoke.plugins.nav(),
+  bespoke.plugins.blackout(),
+  bespoke.plugins.forms()
+]);
+
+// expose API to other applications
+window.deck = deck;
