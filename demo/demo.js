cd30c95ef247a2ddf9a5340935949459b7f4166a
Dan Allen <dan.j.allen@gmail.com>
2016-04-15 23:46:53 -0600

    remove assignment in demo script

--- demo/demo.js
+++ demo/demo.js
@@ -1,9 +1,6 @@
-var deck = bespoke.from('.deck', [
+bespoke.from('.deck', [
   bespoke.plugins.classes(),
   bespoke.plugins.nav(),
   bespoke.plugins.blackout(),
   bespoke.plugins.forms()
 ]);
-
-// expose API to other applications
-window.deck = deck;
